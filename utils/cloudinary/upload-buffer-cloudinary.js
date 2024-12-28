5f5eb8fe67369b93563df47f09c45d062ac2a87e
Dike Wisdom <dikewisdom787@gmail.com>
2024-12-28 18:35:15 -0500

    Updated Server Core Functions

--- utils/cloudinary/upload-buffer-cloudinary.js
+++ utils/cloudinary/upload-buffer-cloudinary.js
@@ -1,63 +1,69 @@
-const { cloudinary } = require("../cloudinary");
+const {cloudinary} = require("../cloudinary");
 
 const uploadBufferCloudinary = async (
-  buffer,
-  transformations,
-  type,
-  public_id,
-  folder
+    buffer,
+    transformations,
+    type,
+    public_id,
+    folder
 ) => {
-  const file = await new Promise((resolve, reject) => {
-    cloudinary.uploader
-      .upload_stream(
-        {
-          folder: folder || "all",
-          public_id,
-          resource_type: type,
-          transformation: transformations,
-          filename_override: public_id,
-        },
-        (error, result) => {
-          if (error) {
-            return reject(error);
-          }
-          resolve(result);
-        }
-      )
-      .end(buffer);
-  });
+    const file = await new Promise((resolve, reject) => {
+        cloudinary.uploader
+            .upload_stream(
+                {
+                    timeout: 60000,
+                    folder: folder || "all",
+                    public_id,
+                    resource_type: type,
+                    transformation: transformations,
+                    filename_override: public_id,
+                },
+                (error, result) => {
+                    if (error) {
+                        return reject(error);
+                    }
+                    resolve(result);
+                }
+            )
+            .end(buffer);
+    });
 
-  return file;
+    return file;
 };
 
 const uploadLargeBufferCloudinary = async (
-  buffer,
-  transformations,
-  type,
-  public_id,
-  folder,
+    buffer,
+    transformations,
+    type,
+    public_id,
+    folder,
 ) => {
-  const file = await new Promise((resolve, reject) => {
-    cloudinary.uploader
-      .upload_stream(
-        {
-          folder: folder || "all",
-          public_id,
-          resource_type: type,
-          transformation: transformations,
-          filename_override: public_id,
-        },
-        (error, result) => {
-          if (error) {
-            return reject(error);
-          }
-          resolve(result);
-        }
-      )
-      .end(buffer);
-  });
+    const file = await new Promise((resolve, reject) => {
+        cloudinary.uploader
+            .upload_stream(
+                {
+                    timeout: 60000,
+                    folder: folder || "all",
+                    public_id,
+                    eager: [
+                        { transformation: 'w_500,h_500', format: 'mp4' }
+                    ],
+                    eager_async: true,
+                    resource_type: type,
+                    transformation: transformations,
+                    filename_override: public_id,
+                },
+                (error, result) => {
+                    if (error) {
+                        return reject(error);
+                    }
+                    resolve(result);
+                }
+            )
+            .end(buffer);
+    });
 
-  return file;
+    return file;
 };
 
-module.exports = { uploadBufferCloudinary, uploadLargeBufferCloudinary };
+module.exports = {uploadBufferCloudinary, uploadLargeBufferCloudinary};
