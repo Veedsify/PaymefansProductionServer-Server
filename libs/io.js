c422943b0289105db68859ae577a9ad47351d8af
DIke Wisdom <dikewisdom787@gmail.com>
2025-03-13 13:48:08 -0400

    Add Socket.IO integration with Redis adapter and implement Cloudflare video upload utility

---
+++ libs/io.js
@@ -0,0 +1,37 @@
+// socketManager.js
+let io;
+const {createAdapter} = require("@socket.io/redis-adapter");
+const redis = require("../../../server/libs/redis-store");
+const {createClient} = require("redis");
+const {ADMIN_PANEL_URL, VERIFICATION_URL, APP_URL, LIVESTREAM_PORT} = process.env;
+const pubClient = createClient({url: "redis://localhost:6379"});
+const subClient = pubClient.duplicate();
+const {Server} = require('socket.io');
+
+module.exports = {
+    init: async (server) => {
+        // Connect to Redis before initializing Socket.IO with the adapter
+        await pubClient.connect();
+        await subClient.connect();
+
+        io = new Server(server, {
+            cors: {
+                origin: [VERIFICATION_URL, ADMIN_PANEL_URL, LIVESTREAM_PORT, APP_URL],
+                methods: ["GET", "POST"],
+            },
+            adapter: createAdapter(pubClient, subClient),
+        });
+
+        // Optional: Handle Redis connection errors
+        pubClient.on('error', (err) => console.error('Redis Pub Error:', err));
+        subClient.on('error', (err) => console.error('Redis Sub Error:', err));
+
+        return io;
+    },
+    getIO: () => {
+        if (!io) {
+            throw new Error('Socket.io not initialized!');
+        }
+        return io;
+    }
+};
